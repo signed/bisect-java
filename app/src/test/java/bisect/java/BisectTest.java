cd81ab4f3cf2aebd4788727ef9dd4611864fd2a3
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-08-22 17:16:13 +0200

    extract helper method

--- app/src/test/java/bisect/java/BisectTest.java
+++ app/src/test/java/bisect/java/BisectTest.java
@@ -4,49 +4,64 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static bisect.java.Bisect.bisect;
-import static bisect.java.BisectResult.bisectResult;
 import static bisect.java.Suspect.suspect;
 import static bisect.java.Version.version;
-import static com.google.common.truth.Truth8.assertThat;
 import static com.google.common.truth.Truth.assertThat;
 
 class BisectTest {
     @Test
     void returnKnownBadIfAllPreviousVesionsAreGood() {
-        BisectOutcome outcome = bisect(version("good"), version("bad"), suspects("good", "bad"));
-        assertThat(outcome.result).hasValue(bisectResult(suspect(version("good")), suspect(version("bad"))));
+        BisectResult success = bisectSuccess("good", "bad", suspects("good", "bad"));
+
+        assertThat(success.lastGood()).isEqualTo(suspect(version("good")));
+        assertThat(success.firstBad()).isEqualTo(suspect(version("bad")));
     }
 
     @Test
     void onlyCheckVersionsBetweenKnownGoodAndKnownBad() {
-        BisectOutcome outcome = bisect(version("good"), version("bad"), suspects("good", "1st bad", "bad"));
-        assertThat(outcome.result).hasValue(bisectResult(suspect(version("good")), suspect(version("1st bad"))));
+        RecordingScene scene = suspects("good", "1st bad", "bad");
+        BisectResult success = bisectSuccess("good", "bad", scene);
+
+        assertThat(success.lastGood()).isEqualTo(suspect(version("good")));
+        assertThat(success.firstBad()).isEqualTo(suspect(version("1st bad")));
     }
 
     @Test
-    void testTheCenterSuspectInsteadOfEverySingleOne(){
+    void testTheCenterSuspectInsteadOfEverySingleOne() {
         RecordingScene scene = suspects("good", "2nd good", "center good", "1st bad", "bad");
-        BisectOutcome outcome = bisect(version("good"), version("bad"), scene);
-        assertThat(outcome.result).hasValue(bisectResult(suspect(version("center good")), suspect(version("1st bad"))));
+        BisectResult success = bisectSuccess("good", "bad", scene);
+
+        assertThat(success.lastGood()).isEqualTo(suspect(version("center good")));
+        assertThat(success.firstBad()).isEqualTo(suspect(version("1st bad")));
         assertThat(scene.checkedVersions).containsExactly(version("center good"), version("1st bad")).inOrder();
     }
 
     @Test
     void skipVersionswhereYouCanNotDecideIfItIsGoodOrBad() {
         RecordingScene scene = suspects("good", "2nd good", "center skip", "1st bad", "bad");
-        BisectOutcome outcome = bisect(version("good"), version("bad"), scene);
-        assertThat(outcome.result).hasValue(bisectResult(suspect(version("2nd good")), suspect(version("1st bad"))));
-        assertThat(scene.checkedVersions).containsExactlyElementsIn(versions("center skip","1st bad", "2nd good")).inOrder();
+        BisectResult success = bisectSuccess("good", "bad", scene);
+
+        assertThat(success.lastGood()).isEqualTo(suspect(version("2nd good")));
+        assertThat(success.firstBad()).isEqualTo(suspect(version("1st bad")));
+        assertThat(scene.checkedVersions).containsExactlyElementsIn(versions("center skip", "1st bad", "2nd good")).inOrder();
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private BisectResult bisectSuccess(String knownGood, String knownBad, RecordingScene scene) {
+        BisectOutcome outcome = bisect(version(knownGood), version(knownBad), scene);
+        Optional<BisectResult> result = outcome.result;
+        return result.orElseThrow();
     }
 
     private List<Version> versions(String... versionStrings) {
         return Arrays.stream(versionStrings).map(Version::version).collect(Collectors.toList());
     }
 
-    private RecordingScene suspects(String ... versions) {
+    private RecordingScene suspects(String... versions) {
         List<Suspect> suspects = Arrays.stream(versions).map(version -> suspect(version(version))).collect(Collectors.toList());
         return new RecordingScene(suspects);
     }
